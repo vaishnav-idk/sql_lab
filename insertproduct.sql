56acc679f9b678a605087b12c7fd5577f97b9243
vaishnav-idk <63421367+vaishnav-idk@users.noreply.github.com>
2023-07-18 06:04:33 +0530

    Update insertproduct.sql

--- insertproduct.sql
+++ insertproduct.sql
@@ -1,1 +1,26 @@
+create database store;
+use store;
 
+create table product(pdtid varchar(20) primary key,
+pname varchar(20),
+price numeric(10,2),
+qtyinstock int);
+
+DELIMITER //
+create procedure insertproduct(
+	in pdt_id varchar(20), 
+	in p_name varchar(20),
+    in p_price numeric(10,2),
+    in quantity int)
+begin
+IF quantity > 0 then
+INSERT INTO product (pdtid,pname,price,qtyinstock) VALUES (pdt_id,p_name,p_price,quantity) ;
+END IF;
+end //
+DELIMITER ;
+
+truncate table product;
+CALL insertproduct('123','Pen',10.33,500);
+CALL insertproduct('124','Pencil',5,0);
+
+select * from product;
